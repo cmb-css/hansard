86801a481f9889709bac2e83e70bf9bfaa5ecd32
Telmo Menezes <telmo@telmomenezes.net>
2021-09-02 16:31:23 +0200

    check if data already exists; start with a given page

--- hansard/cli.py
+++ hansard/cli.py
@@ -17,17 +17,22 @@ def cli():
     parser.add_argument('--end_date', type=str,
                         help='latest debate date',
                         default='31/08/2021')
+    parser.add_argument('--page', type=int,
+                        help='search result page to start with',
+                        default=1)
     args = parser.parse_args()
 
     print('search term: {}'.format(args.search))
     print('output directory: {}'.format(args.outdir))
     print('start date: {}'.format(args.start_date))
     print('end date: {}'.format(args.end_date))
+    print('page: {}').format(args.page)
 
     if args.command == 'retrieve':
         retrieve(search_term=args.search,
                  outdir=args.outdir,
                  start_date=args.start_date,
-                 end_date=args.end_date)
+                 end_date=args.end_date,
+                 page=args.page)
     else:
         print('Unknown command: {}'.format(args.command))
--- hansard/retrieve.py
+++ hansard/retrieve.py
@@ -1,17 +1,21 @@
 import json
 import requests
+import time
 
 from bs4 import BeautifulSoup
+from pathlib import Path
 from requests.exceptions import HTTPError
 
 
 BASE_URL = 'https://hansard.parliament.uk'
+SLEEP_TIME = 60
 
 
-def retrieve(search_term, outdir, start_date, end_date):
+def retrieve(search_term, outdir, start_date, end_date, page=1):
     finished = False
-    i = 1
+    i = page
     debates = 0
+    repeated = 0
     errors = 0
     while not finished:
         args = {'searchTerm': search_term,
@@ -21,53 +25,58 @@ def retrieve(search_term, outdir, start_date, end_date):
                 'page': i}
         args_str = '&'.join('{}={}'.format(key, args[key]) for key in args)
         url = '{}/search/Contributions?{}'.format(BASE_URL, args_str)
-        response = requests.get(url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.content, 'lxml')
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            soup = BeautifulSoup(response.content, 'lxml')
     
-        results = soup.find_all('a', class_='card card-calendar')
-        if len(results) == 0:
-            finished = True
+            results = soup.find_all('a', class_='card card-calendar')
+            if len(results) == 0:
+                finished = True
     
-        for result in results:
-            uparts = result['href'].split('/')
-            if len(uparts) > 4 and uparts[3] == 'debates':
-                debate = {'url': '{}{}'.format(BASE_URL, result['href']),
-                          'house': uparts[1],
-                          'date': uparts[2],
-                          'code': uparts[4]}
-
-                txt_url = '{}/debates/GetDebateAsText/{}'.format(
-                    BASE_URL, debate['code'])
-            
-                try:
-                    response = requests.get(txt_url)
-                    response.raise_for_status()
-                    text = response.content.decode('utf-8')
-                    debate['text'] = text
-
-                    matches = result.find_all('div', class_='primary-info')
-                    for match in matches:
-                        debate['primary-info'] = match.get_text().strip()
-                    matches = result.find_all('div', class_='secondary-info')
-                    for match in matches:
-                        debate['secondary-info'] = match.get_text().strip()
+            for result in results:
+                uparts = result['href'].split('/')
+                if len(uparts) > 4 and uparts[3] == 'debates':
+                    debate = {'url': '{}{}'.format(BASE_URL, result['href']),
+                              'house': uparts[1],
+                              'date': uparts[2],
+                              'code': uparts[4]}
 
                     filepath = '{}/{}_{}.json'.format(outdir,
                                                       debate['date'],
                                                       debate['code'])
-                    with open(filepath, 'w', encoding='utf-8') as f:
-                        json.dump(debate, f, ensure_ascii=False, indent=4)
-                    debates += 1
-                    # print(json.dumps(record, ensure_ascii=False, indent=4))
-                except HTTPError as http_err:
-                    print('HTTP error occurred: {}'.format(http_err))
-                    print(debate['url'])
-                    errors += 1
-            else:
-                print('ERROR #1')
-                print(result['href'])
-                errors += 1
 
-        print('page {}; debates: {}; errors: {}'.format(i, debates, errors))
-        i += 1
+                    if Path(filepath).exists():
+                        repeated += 1
+                    else:
+                        txt_url = '{}/debates/GetDebateAsText/{}'.format(
+                            BASE_URL, debate['code'])
+            
+                        try:
+                            response = requests.get(txt_url)
+                            response.raise_for_status()
+                            text = response.content.decode('utf-8')
+                            debate['text'] = text
+
+                            with open(filepath, 'w', encoding='utf-8') as f:
+                                json.dump(
+                                    debate, f, ensure_ascii=False, indent=4)
+                            debates += 1
+                        except HTTPError as http_err:
+                            print('HTTP error occurred: {}'.format(http_err))
+                            print(debate['url'])
+                            errors += 1
+                else:
+                    print('ERROR #1')
+                    print(result['href'])
+                    errors += 1
+
+            print('page {}; debates: {}; repeated: {}; errors: {}'.format(
+                i, debates, repeated, errors))
+            i += 1
+        except HTTPError as http_err:
+            print('HTTP error occurred: {}'.format(http_err))
+            print(url)
+            print('Going to sleep for {} seconds...'.format(SLEEP_TIME))
+            time.sleep(SLEEP_TIME)
+    print('done.')
